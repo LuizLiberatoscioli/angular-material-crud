25c5ab31dbfe24fa023e70307b40e9f5fdea25fd
LuizPaulo <luizhat10@gmail.com>
2025-08-31 11:32:38 -0400

    update

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -6,6 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { Cliente } from './cliente';
 
 
 @Component({
@@ -22,5 +23,11 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+
+  cliente: Cliente = Cliente.newCliente();
+
+  salvar(){
+    console.log("Dados Cliente: " , this.cliente);
+  }
 
 }
